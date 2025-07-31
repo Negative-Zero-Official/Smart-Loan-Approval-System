40302671ba8000f9f1df920982f1a5141df2d31c
Prahlad Gaitonde <pgaitonde05@gmail.com>
2025-07-31 23:18:09 +0530

    Refactor form data collection and API prediction handling for improved clarity and structure
    NOTE: frontend is broken. WIP

--- ml_model/api.py
+++ ml_model/api.py
@@ -5,21 +5,28 @@ import pandas as pd
 app = Flask(__name__)
 
 model = joblib.load("ml_model\\loan_model.pkl")
-model_columns = joblib.load("ml_model\\model_columns.pkl")
 
-@app.route("/predict", methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
-        input_df = pd.DataFrame([data], columns=model_columns)
+        print("Raw input:", data)
+
+        model_columns = joblib.load("ml_model\\model_columns.pkl")
+        clean_data = {key: data[key] for key in model_columns if key in data}
+
+        print("Filtered input:", clean_data)
+        input_df = pd.DataFrame([clean_data], columns=model_columns)
 
         prediction = model.predict(input_df)[0]
         return jsonify({
-            "prediction" : int(prediction),
-            "status" : "Approved" if prediction == 1 else "Rejected"
+            "prediction": int(prediction),
+            "status": "Approved" if prediction == 1 else "Rejected"
         })
+
     except Exception as e:
-        return jsonify({"error" : str(e)})
+        return jsonify({"error": str(e)})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
